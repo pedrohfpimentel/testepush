736f3f8deec2aafef5bc5986853f7850c6a78618
pedrohfpimentel <pedrohfpimentel@gmail.com>
2019-01-09 14:55:41 -0200

    professional export e export history funcionando

--- app/src/Model/Professional.php
+++ app/src/Model/Professional.php
@@ -8,13 +8,26 @@ class Professional
     public $id;
     public $id_user;
     public $id_professional_type;
-
+    public $name;
+    public $email;
+    public $cpf;
+    public $tel_area;
+    public $tel_numero;
+    public $end_cep;
+    public $professional_type_name;
 
     public function __construct(array $data = [])
     {
         $this->id = $data['id'] ?? null;
         $this->id_user = $data['id_user'] ?? null;
         $this->id_professional_type = $data['id_professional_type'] ?? null;
+        $this->name = $data['name'] ?? null;
+        $this->email = $data['email'] ?? null;
+        $this->cpf = $data['cpf'] ?? null;
+        $this->tel_area = $data['tel_area'] ?? null;
+        $this->tel_numero = $data['tel_numero'] ?? null;
+        $this->end_cep = $data['end_cep'] ?? null;
+        $this->professional_type_name = $data['professional_type_name'] ?? null;
 
     }
 }
