61cf5f502e9551cc73c8e5bb772afe9cf67ebec9
Thiago Nardi <thnardi@gmail.com>
2024-09-05 03:53:14 -0300

    Update User.php

--- app/src/User.php
+++ app/src/User.php
@@ -263,16 +263,16 @@ class User
     	 // CPF é válido
     	 } else {
 
-    		for ($t = 9; $t < 11; $t++) {
+    		// for ($t = 9; $t < 11; $t++) {
 
-    			for ($d = 0, $c = 0; $c < $t; $c++) {
-    				$d += $cpf{$c} * (($t + 1) - $c);
-    			}
-    			$d = ((10 * $d) % 11) % 10;
-    			if ($cpf{$c} != $d) {
-    				return false;
-    			}
-    		}
+    		// 	for ($d = 0, $c = 0; $c < $t; $c++) {
+    		// 		$d += $cpf{$c} * (($t + 1) - $c);
+    		// 	}
+    		// 	$d = ((10 * $d) % 11) % 10;
+    		// 	if ($cpf{$c} != $d) {
+    		// 		return false;
+    		// 	}
+    		// }
 
     		return true;
     	}
